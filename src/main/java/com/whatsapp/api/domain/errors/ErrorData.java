5efc313a03c3da6133dd68ea01109e4ce15094b2
Mauricio Binda da Costa <mauriciobinda@hotmail.com>
2023-02-24 14:25:27 -0300

    wip - javadoc fix

--- src/main/java/com/whatsapp/api/domain/errors/ErrorData.java
+++ src/main/java/com/whatsapp/api/domain/errors/ErrorData.java
@@ -3,6 +3,9 @@ package com.whatsapp.api.domain.errors;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * The type Error data.
+ */
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record ErrorData(@JsonProperty("messaging_product") String messagingProduct,
                         @JsonProperty("details") String details,
--- src/main/java/com/whatsapp/api/domain/errors/WhatsappApiError.java
+++ src/main/java/com/whatsapp/api/domain/errors/WhatsappApiError.java
@@ -3,6 +3,9 @@ package com.whatsapp.api.domain.errors;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * The type Whatsapp api error.
+ */
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record WhatsappApiError(
         @JsonProperty("error")
