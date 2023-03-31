ea2274b7503c907e24c44e4fb62c92c18c0401a3
Pratik Soni <100229748+PratikLaweb@users.noreply.github.com>
2023-03-30 21:49:35 -0400

    Added Location message.

---
+++ src/test/java/com/whatsapp/api/examples/SendLocationMessageExample.java
@@ -0,0 +1,34 @@
+package com.whatsapp.api.examples;
+
+import static com.whatsapp.api.TestConstants.PHONE_NUMBER_1;
+import static com.whatsapp.api.TestConstants.PHONE_NUMBER_ID;
+import static com.whatsapp.api.TestConstants.TOKEN;
+
+import com.whatsapp.api.WhatsappApiFactory;
+import com.whatsapp.api.domain.messages.LocationMessage;
+import com.whatsapp.api.domain.messages.Message.MessageBuilder;
+import com.whatsapp.api.domain.messages.response.MessageResponse;
+import com.whatsapp.api.impl.WhatsappBusinessCloudApi;
+
+public class SendLocationMessageExample {
+
+	public static void main(String[] args) {
+		
+		WhatsappApiFactory factory = WhatsappApiFactory.newInstance(TOKEN);
+
+        WhatsappBusinessCloudApi whatsappBusinessCloudApi = factory.newBusinessCloudApi();
+        var locationMessage = new LocationMessage()//
+                .setLongitude("38.819830")
+                .setLatitude("-77.151700");
+
+        var message = MessageBuilder.builder()//
+                .setTo(PHONE_NUMBER_1)//
+                .buildLocationMessage(locationMessage);
+
+        MessageResponse messageResponse = whatsappBusinessCloudApi.sendMessage(PHONE_NUMBER_ID, message);
+
+        System.out.println(messageResponse);
+
+	}
+
+}
