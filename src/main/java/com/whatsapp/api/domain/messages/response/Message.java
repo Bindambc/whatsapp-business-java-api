9f0d79dbb216889c1b6ffc59c84132cf52fa14ea
Zhou <lilaboc.cn@gmail.com>
2023-09-30 16:16:24 +0800

    fix https://github.com/Bindambc/whatsapp-business-java-api/issues/125

--- src/main/java/com/whatsapp/api/domain/messages/response/Message.java
+++ src/main/java/com/whatsapp/api/domain/messages/response/Message.java
@@ -8,6 +8,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  */
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record Message(
-
-        @JsonProperty("id") String id) {
+        @JsonProperty("id") String id,
+        @JsonProperty("message_status") String messageStatus){
 }
